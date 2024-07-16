140b3e5fede33215f0c30339406e1e9629924725
maitalata <maitalata@gmail.com>
2024-07-16 17:57:10 +0100

    added axios and break down the app into components

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer class="py-3 my-4">
+        <ul class="pb-3 mb-3 nav justify-content-center border-bottom">
+            <li class="nav-item">
+                <a href="#" class="px-2 nav-link text-muted">Term</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="px-2 nav-link text-muted">Privacy</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="px-2 nav-link text-muted">FAQs</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="px-2 nav-link text-muted">About</a>
+            </li>
+        </ul>
+        <p class="text-center text-muted">&copy; 2023 ToeDoeList</p>
+    </footer>
+</template>
---
+++ src/components/tasks/Task.vue
@@ -0,0 +1,31 @@
+<template>
+    <li class="py-3 list-group-item" >
+        <div class="d-flex justify-content-start align-items-center">
+            <input class="mt-0 form-check-input completed" type="checkbox" />
+            <div class="ms-2 flex-grow-1" title="Double click the text to edit or remove">
+                <!-- <div class="relative">
+                    <input class="editable-task" type="text" />
+                </div> -->
+                <span>{{ task.name }}</span>
+            </div>
+            <!-- <div class="task-date">24 Feb 12:00</div> -->
+        </div>
+        <div class="task-actions">
+            <button class="btn btn-sm btn-circle btn-outline-secondary me-1">
+                <IconPencil />
+            </button>
+            <button class="btn btn-sm btn-circle btn-outline-danger">
+                <IconTrash />
+            </button>
+        </div>
+    </li>
+</template>
+
+<script setup>
+import IconPencil from "../icons/IconPencil.vue"
+import IconTrash from "../icons/IconTrash.vue"
+
+defineProps({
+    task: Object
+})
+</script>
